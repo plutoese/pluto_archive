918772cd983feb17141269961f28ff33aac9a84a
Plutoese <glen.zhang7@gmail.com>
2015-09-08 14:54:07 +0800

    regional data and layout class

--- Program/Python/library/datapretreatment/class_DataSet.py
+++ Program/Python/library/datapretreatment/class_DataSet.py
@@ -1,11 +1,6 @@
 # coding=UTF-8
 
-import json
-import pickle
-import pandas as pd
-import numpy as np
 from library.region.class_AdminCode import *
-from library.datapretreatment.class_DataSheet import *
 
 # 类DataSet代表数据集
 # 用来生成和转换数据
@@ -26,64 +21,6 @@ class DataSet:
 
     # subset of the dataset
     def setSubset(self,year=None,region=None,variable=None,mindex=None):
-        if year is None:
-            year = slice(None)
-        else:
-            if isinstance(year,(range,list)):
-                year = [str(y) for y in year]
-        if region is None:
-            region = slice(None)
-        else:
-            regions = []
-            for item in region:
-                if len(item) == 1:
-                    regions.append(self.AD[item[0]]['acode'])
-                else:
-                    regions.append(self.AD[tuple(item)]['acode'])
-            region = regions
-        if variable is None:
-            variable = slice(None)
-
-        self.data = self._data
-
-        if self.data.ndim == 1:
-            self.data = self.data.loc[year]
-
-        elif self.data.ndim == 2:
-            if year in mindex and region in mindex:
-                self.data = self.data.loc[region,year]
-            if year in mindex and variable in mindex:
-                self.data = self.data.loc[year,variable]
-            if region in mindex and variable in mindex:
-                self.data = self.data.loc[region,variable]
-
-        else:
-            self.data = self.data.loc[year,region,variable]
-
-        # transform
-        shape = self.data.shape
-        if len(shape) == 3:
-            if shape[0] == 1:
-                self.data = self.data.xs(key=year,axis=0)
-            if len(self.data.shape) < 3:
-                if shape[1] == 1:
-                    self.data = self.data.xs(key=region[0],axis=0)
-                if len(self.data.shape) < 2:
-                    if shape[2] == 1:
-                        self.data = self.data.xs(key=variable[0],axis=0)
-                else:
-                    if shape[2] == 1:
-                        self.data = self.data.xs(key=variable[0],axis=1)
-            else:
-                if shape[1] == 1:
-                    self.data = self.data.xs(key=region[0],axis=1)
-                if len(self.data.shape) < 3:
-                    if shape[2] == 1:
-                        pass
-                else:
-                    if shape[2] == 1:
-                        self.data = self.data.xs(key=variable[0],axis=2)
-
         shape = self.data.shape
         if len(shape) == 1:
             self.data = self.data.apply(self._setMissingData)
@@ -106,64 +43,6 @@ class DataSet:
         else:
             return x
 
-# 类ToDataSet用来转换数据表到数据集
-class PandasDataStruct:
-    # 构造函数
-    def __init__(self,data=None):
-        # load data
-        self._data = data
-        self.vars = self._data[0][3:]
-        self.rdata = self._data[1:]
-        self.rows = len(self.rdata)
-        self.period = list(sorted(set([int(item[2]) for item in self.rdata])))
-        self.numberofdataframe = int(self.rows / len(self.period))
-
-    # to Pandas DataStruct
-    def toPDataStruct(self):
-        nvar = len(self.vars)
-        nyear = len(self.period)
-        nunit = self.numberofdataframe
-
-        if nvar > 1 & nyear > 1 & nunit > 1:
-            return self._toPanel
-
-
-    # 转换为Panel数据
-    def _toPanel(self):
-        regions = [self.rdata[n][0] for n in range(self.numberofdataframe)]
-        variables = self.vars
-        ndata = dict()
-        for i in range(len(self.period)):
-            mdata = [self.rdata[j][3:] for j in range(i * self.numberofdataframe, (i + 1) * self.numberofdataframe)]
-            dframe = pd.DataFrame(mdata,columns=variables,index=regions)
-            ndata[str(self.period[i])] = dframe
-        paneldata = pd.Panel(ndata)
-
-        return paneldata
-
 if __name__ == '__main__':
-    #mdatasheet = DataSheet(r'C:\Room\Warehouse\GitWork\Program\Python\library\dataset\CEICData.xlsx')
-    #json.dump(mdatasheet.data, fp=open(r'C:\Room\Warehouse\GitWork\Program\Python\library\dataset\CEICData.txt','w'))
-    '''
-    P = json.load(open(r'C:\Room\Warehouse\GitWork\Program\Python\library\dataset\CEICData.txt'))
-    topanel = PandasDataStruct(P)
-    pdata = topanel._toPanel()
-    F = open(r'C:\Room\Warehouse\GitWork\Program\Python\library\dataset\CEICPdata.pkl','wb')
-    pickle.dump(pdata,F)
-    F.close()
-    '''
-    F = open(r'C:\Room\Warehouse\GitWork\Program\Python\library\dataset\CEICPdata.pkl','rb')
-    pdata = pickle.load(F)
-    dset = DataSet(pdata)
-    dset.setSubset(year=range(2000,2013),region=[[u'上海']],variable=[u'国内生产总值_人均'])
-    #dset.setSubset(year='2012',region=[[u'北京'],[u'上海'],[u'浙江',u'嘉兴']],variable=[u'财政支出',u'国内生产总值',u'国内生产总值_人均'])
-    #dset.setSubset(year=range(2010,2013),region=[[u'北京'],[u'上海'],[u'浙江',u'嘉兴']],variable=[u'财政支出',u'国内生产总值',u'国内生产总值_人均'])
-    #dset.setSubset(year=range(2000,2013),region=[[u'北京']],variable=[u'财政支出',u'国内生产总值',u'国内生产总值_人均'])
-    #dset.setSubset(year=range(2000,2014),region=[[u'北京'],[u'上海'],[u'浙江',u'杭州']],variable=[u'国内生产总值_人均'])
-    dset.noNA()
-    mdata = dset.data
-
-    print(mdata)
-    print(dset.data.ndim)
-
+    pass
     
--- Program/Python/library/region/class_Layout.py
+++ Program/Python/library/region/class_Layout.py
@@ -1,44 +1,128 @@
 # coding=UTF-8
 
 from library.region.class_RegionalData import *
+import pandas as pd
 
 # 类Layout用于各种数据结构的转换
 class Layout:
     # 构造函数
     def __init__(self,data=None):
+        self.ad = AdminCode()
         self._data = data
-        self._determType()
+        self._type()
+
+        self.type = self._type()
+        self.ndim = {'year':len(self.type['year']),'variable':len(self.type['variable']),'region':len(self.type['region'])}
 
     # 格式转换
     def stackToNormal(self):
-        pass
+        #  构建时间序列数据
+        if (self.ndim['variable'] == 1) and (self.ndim['region'] == 1):
+            #data = pd.Series(dict(zip(year,[group for name, group in self._data.groupby(['acode','variable'],sort=True)][0]['value'])))
+            #d = dict(zip(year,data))
+            sname = '|'.join([self.region[0],self.variable[0]])
+            return pd.Series(dict(zip(self.year,[group for name, group in self._data.groupby(['acode','variable'],sort=True)][0]['value'])),name=sname)
 
-    def _determType(self):
-        g = mdata.groupby(['year'], sort=True)
+        # 构建横截面数据(地区|变量)
+        if self.ndim['year'] == 1:
+            g1 = [group for name,group in self._data.groupby(['year'], sort=True)][0]
+            g2 = g1.groupby(['variable'], sort=True)
+            i = 0
+            mdata = []
+            for name,data in g2:
+                rdata = pd.DataFrame({name:data['value'].values},index=data['acode'].values)
+                if i == 0:
+                    mdata = rdata
+                else:
+                    mdata = pd.merge(mdata,rdata,left_index=True,right_index=True,how='outer')
+                i = i + 1
+            mdata.insert(0, 'region', [self.ad.getByAcode(item)['region'] for item in mdata.index])
+            return mdata
+
+        # 构建横截面数据(地区|时间)
+        if self.ndim['variable'] == 1:
+            g = self._data.groupby(['year'], sort=True)
+            i = 0
+            mdata = []
+            for name,data in g:
+                rdata = pd.DataFrame({name:data['value'].values},index=data['acode'].values)
+                if i == 0:
+                    mdata = rdata
+                else:
+                    mdata = pd.merge(mdata,rdata,left_index=True,right_index=True,how='outer')
+                i = i + 1
+            mdata.insert(0, 'region', [self.ad.getByAcode(item)['region'] for item in mdata.index])
+            return mdata
+
+        # panel data
+        g = self._data.groupby(['year'], sort=True)
+        year = []
+        pdata =[]
+        for y,g1 in g:
+            g2 = g1.groupby(['variable'], sort=True)
+            i = 0
+            mdata = []
+            for name,data in g2:
+                rdata = pd.DataFrame({name:data['value'].values},index=data['acode'].values)
+                if i == 0:
+                    mdata = rdata
+                else:
+                    mdata = pd.merge(mdata,rdata,left_index=True,right_index=True,how='outer')
+                i = i + 1
+            mdata.insert(0, 'region', [self.ad.getByAcode(item)['region'] for item in mdata.index])
+            year.append(str(y))
+            pdata.append(mdata)
+        result = pd.Panel(dict(zip(year,pdata)))
+        return result
+
+
+    # 辅助函数，返回数据结构
+    def _type(self):
+        g = self._data.groupby(['year'], sort=True)
         self.year = [name for name, group in g]
+        self.year = [str(item) for item in self.year]
 
-        g = mdata.groupby(['acode'], sort=True)
-        self.region = [name for name, group in g]
+        g = self._data.groupby(['acode'], sort=True)
+        self.acode = [name for name, group in g]
+        self.region = [ad.getByAcode(item)['region'] for item in self.acode]
 
-        g = mdata.groupby(['variable'], sort=True)
+        g = self._data.groupby(['variable'], sort=True)
         self.variable = [name for name, group in g]
 
-
-
+        return {'year':self.year,'region':self.region,'variable':self.variable}
 
 if __name__ == '__main__':
     ad = AdminCode()
     rdata = RegionalData()
-    #mdata = rdata.query(region=ad[u'浙江',u'f'],year=range(2006,2010),variable=[u'财政支出',u'从业人数_在岗职工'])
+    mdata = rdata.query(region=ad[u'浙江',u'f'],year=range(2006,2010),variable=[u'财政支出',u'从业人数_在岗职工'])
     #mdata = rdata.query(region=[ad[u'浙江',u'杭州']],variable=[u'财政支出'])
-    #mdata = rdata.query(region=ad[u'浙江',u'f'],variable=u'财政支出',year=2012)
-    mdata = rdata.query(region=ad[u'浙江',u'杭州'],variable=u'财政支出',year=2012)
+    #mdata = rdata.query(region=ad[u'浙江',u'f'],variable=[u'财政支出'],year=range(2005,2013))
+    #mdata = rdata.query(region=ad[u'浙江',u'杭州'],variable=u'财政支出',year=range(2000,2012))
     lout = Layout(mdata)
+    print(lout.type)
+    print(lout.ndim)
 
-    g = mdata.groupby(['acode'], sort=True)
+    print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    g = mdata.groupby(['year'], sort=True)
     print(g)
 
     for name, group in g:
         print(name)
         print(group)
         print(group['value'])
+
+    print('*****************************************')
+    g = group.groupby(['variable'], sort=True)
+    i = 0
+    for name, group in g:
+        print('not',i)
+        print(name)
+        print(group)
+        i = i + 1
+
+    print('-----------------------------------------')
+    mdata = lout.stackToNormal()
+    print(mdata)
+    print(mdata.axes)
+    print(mdata['2007'])
+
