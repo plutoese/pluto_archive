b85f0821222966b49afb07ee7c0dad1a0a01f1b0
Plutoese <glen.zhang7@gmail.com>
2015-10-12 12:16:19 +0800

    report version 0.2

--- Program/Python/library/report/demo_basic.py
+++ Program/Python/library/report/demo_basic.py
@@ -1,42 +1,38 @@
 # coding=UTF-8
 
-import numpy as np
+from pylatex.command import Command
+from pylatex import Document, Section, Subsection, Package
+from pylatex.utils import italic,escape_latex
 
-from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, \
-    Plot, Figure, Package
-from pylatex.utils import italic
-import os
+def fill_document(doc):
+    """Add a section, a subsection and some text to the document.
+
+    :param doc: the document
+    :type doc: :class:`pylatex.document.Document` instance
+    """
+    s = u'地区'
+
+    with doc.create(Section('A section')):
+        doc.append('Some regular text and some ')
+        doc.append(italic('italic text. '))
+
+        with doc.create(Subsection('A subsection')):
+            doc.append(s)
+            doc.append(escape_latex('Also some crazy characters: $&#{}'))
+
 
 if __name__ == '__main__':
 
+     # Document with `\maketitle` command activated
     doc = Document()
-    doc.packages.append(Package('geometry', options=['tmargin=1cm',
-                                                     'lmargin=10cm']))
+    doc.packages.append(Package('ctex',options=['UTF8','noindent']))
+    doc.preamble.append(Command('title', 'Awesome Title'))
+    doc.preamble.append(Command('author', 'Anonymous author'))
+    doc.preamble.append(Command('date', r'\today'))
+    doc.append(r'\maketitle')
 
-    with doc.create(Section('The simple stuff')):
-        doc.append('Some regular text and some')
-        doc.append(italic('italic text. '))
-        doc.append('\nAlso some crazy characters: $&#{}')
-        with doc.create(Subsection('Math that is incorrect')):
-            doc.append(Math(data=['2*3', '=', 9]))
-
-        with doc.create(Subsection('Table of something')):
-            with doc.create(Tabular('rc|cl')) as table:
-                table.add_hline()
-                table.add_row((1, 2, 3, 4))
-                table.add_hline(1, 2)
-                table.add_empty_row()
-                table.add_row((4, 5, 6, 7))
-
-    a = np.array([[100, 10, 20]]).T
-
-
-
-    doc.generate_pdf('full')
-
-
-
-
+    fill_document(doc)
+    doc.generate_pdf('c:\\down\\basic_maketitle')
 
 
 
